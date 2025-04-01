23e210a32244348440083f8f3f4dedfc38c80ea7
Sahnawas Hussain <142085109+sahnawashussain786@users.noreply.github.com>
2025-04-01 12:32:06 +0530

    Add Clerk integration and new PromptBox component; update Sidebar for user profile access

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,8 +1,12 @@
-import { assets } from "@/assets/assets";
 import Image from "next/image";
 import React from "react";
+import { assets } from "@/assets/assets";
+import { useClerk, UserButton } from "@clerk/nextjs"; // Ensure this import exists
 
 const Sidebar = ({ expand, setExpand }) => {
+
+  const {openSignIn} = useClerk()
+
   return (
     <div
       className={`flex flex-col justify-between bg-[#212327] pt-7 transition-all z-50 max-md:absolute max-md:h-screen ${
@@ -21,7 +25,7 @@ const Sidebar = ({ expand, setExpand }) => {
             src={expand ? assets.logo_text : assets.logo_icon}
           />
           <div
-            onClick={() => (expand ? setExpand(false) : setExpand(true))}
+            onClick={() => setExpand(!expand)}
             className="group relative flex items-center justify-center hover:bg-gray-500/20 transition-all duration-300 h-9 w-9 aspect-square rounded-lg cursor-pointer"
           >
             <Image alt="" src={assets.menu_icon} className="md:hidden" />
@@ -33,9 +37,9 @@ const Sidebar = ({ expand, setExpand }) => {
             <div
               className={`absolute w-max ${
                 expand ? "left-1/2 -translate-x-1/2 top-12" : "-top-12 left-0"
-              } opacity-0 group-hover:opacity-100 transition bg-black text-white text-sm px-3 py-2 rounded-lg shadown-lg pointer-events-none`}
+              } opacity-0 group-hover:opacity-100 transition bg-black text-white text-sm px-3 py-2 rounded-lg shadow-lg pointer-events-none`}
             >
-              {expand ? "close sidebar" : "open sidebar"}
+              {expand ? "Close Sidebar" : "Open Sidebar"}
               <div
                 className={`w-3 h-3 absolute bg-black rotate-45 ${
                   expand
@@ -46,6 +50,7 @@ const Sidebar = ({ expand, setExpand }) => {
             </div>
           </div>
         </div>
+
         <button
           className={`mt-8 flex items-center justify-center cursor-pointer ${
             expand
@@ -64,17 +69,22 @@ const Sidebar = ({ expand, setExpand }) => {
           </div>
           {expand && <p className="text-white text font-medium">New Chat</p>}
         </button>
+
+        {expand && (
+          <div className="mt-8 text-white/25 text-sm">
+            <p className="my-1">Recents</p>
+          </div>
+        )}
+      </div>
+
+      <div>
         <div
-          className={`mt-8 text-white/25 text-sm ${
-            expand ? "block" : "hidden"
+          className={`flex items-center cursor-pointer group relative ${
+            expand
+              ? "gap-1 text-white-80 text-sm p-2.5 border border-primary rounded-lg hover:bg-white/10 cursor-pointer"
+              : "h-10 w-10 mx-auto hover:bg-gray-500/30 rounded-lg"
           }`}
         >
-          <p className="my-1">Recents</p>
-          {/* chat label */}
-        </div>
-      </div>
-      <div>
-        <div className={`flex items-center cursor-pointer group relative ${expand ? 'gap-1 text-white-80 text-sm p-2.5 border border-primary rounded-lg hover:bg-white/10 cursor-pointer' : 'h-10 w-10 mx-auto hover:bg-gray-500/30 rounded-lg'}`}>
           <Image
             src={expand ? assets.phone_icon : assets.phone_icon_dull}
             alt=""
@@ -100,6 +110,16 @@ const Sidebar = ({ expand, setExpand }) => {
               <span>Get App</span> <Image src={assets.new_icon} alt="" />
             </>
           )}
+        </div>
+
+        <div
+          onClick={openSignIn}
+          className={`flex items-center ${
+            expand ? "hover:bg-white/10 rounded-lg" : "justify-center w-full"
+          } gap-3 text-white/60 text-sm p-2 mt-2 cursor-pointer`}
+        >
+          <Image src={assets.profile_icon} className="w-7" alt="" />
+          {expand && <span>My Profile</span>}
         </div>
       </div>
     </div>
